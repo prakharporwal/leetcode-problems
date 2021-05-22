f9bd9c3ca7f6f95ebdd6df6547bdd056bbfb727c
prakharporwal <prakharporwal99@gmail.com>
2021-05-22 20:03:32 +0530

    vscode config to run cpp code and leetcode template

---
+++ problems/4. Median of 2 Arrays.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    //// your code goes here paste your function here
+    /// int function(){}
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // 0...mid | mid+1,n-1
+        int mid1=0,mid2=0;
+        
+        
+        if( abs(2*mid1 + 2*mid2+4 - n-m  )<=1 && max(nums1[mid1],nums2[mid2]) < min(nums1[mid1+1],nums2[mid2+1])){
+            
+        }
+    }
+};
+
+
+
+int main(){
+    Solution sol ;
+
+    vector<int> nums1 = {} ;
+    vector<int> nums2 = {} ;
+    //print function output here ......
+    cout << sol.runcode() << endl;
+    
+    return 0;
+}
+
+1-> 2-> 3-> 4-> 5->6   ==> 3,5
+
+1-> 
